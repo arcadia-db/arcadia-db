27c31ffd5c87cf8b8e87e75cdd8d0194f357987b
Rajat Patwari <rajatpatwari@gmail.com>
2022-12-13 23:57:59 -0500

    feat: BPM MVP, in progress with concurrency issues

--- src/src/buffer_pool_manager/mod.rs
+++ src/src/buffer_pool_manager/mod.rs
@@ -1,93 +1,81 @@
-use self::buffer_pool::*;
-use crate::shared::{config::*, contracts::*, definitions::*};
-use std::{collections::*, sync::*};
+use self::{
+    buffer_pool::{contracts::BufferPool, BufferPoolImpl},
+    contracts::BufferPoolManager,
+};
+use crate::shared::{config::BufferPoolManagerConfig, definitions::TableId};
+use parking_lot::RwLock;
+use std::{collections::HashMap, sync::Arc};
+
+pub mod contracts {
+    use super::*;
+
+    pub use buffer_pool::contracts::*;
+
+    pub trait BufferPoolManager: Send + Sync + 'static {
+        fn contains_unique_buffer_pool(&self, table_id: TableId) -> bool;
+        fn get_buffer_pool(&self, table_id: TableId) -> Arc<dyn BufferPool>;
+        /// # Safety
+        /// The buffer pool manager must not contain `table_id`.
+        fn create_unique_buffer_pool(
+            &self,
+            table_id: TableId,
+            page_size: usize,
+            entries_count: usize,
+        );
+    }
+
+    pub fn create_default_buffer_pool_manager(
+        config: BufferPoolManagerConfig,
+    ) -> Arc<dyn BufferPoolManager> {
+        Arc::new(BufferPoolManagerImpl::new(config))
+    }
+}
 
 mod buffer_pool;
 
 #[derive(Debug)]
-pub struct BufferPoolManagerImpl {
-    config: BufferPoolManagerConfig,
-    pools: RwLock<HashMap<TableId, Arc<BufferPoolImpl>>>,
+/* private */
+struct BufferPoolManagerImpl {
+    default_buffer_pool: Arc<BufferPoolImpl>,
+    unique_buffer_pools: RwLock<HashMap<TableId, Arc<BufferPoolImpl>>>,
 }
 
 impl BufferPoolManagerImpl {
     pub fn new(config: BufferPoolManagerConfig) -> Self {
         Self {
-            config,
-            pools: RwLock::new(HashMap::new()),
+            default_buffer_pool: Arc::new(BufferPoolImpl::new(
+                config.default_page_size,
+                config.default_entries_count,
+            )),
+            unique_buffer_pools: RwLock::new(HashMap::new()),
         }
     }
 }
 
 impl BufferPoolManager for BufferPoolManagerImpl {
-    fn contains_buffer_pool(&self, table_id: TableId) -> bool {
-        let pools = self.pools.read().unwrap();
-        pools.contains_key(&table_id)
-    }
-
-    fn create_buffer_pool(
-        &self,
-        table_id: TableId,
-        page_size: Option<usize>,
-        entries_count: Option<usize>,
-    ) {
-        let page_size = if let Some(page_size) = page_size {
-            page_size
-        } else {
-            self.config.default_page_size
-        };
-
-        let entries_count = if let Some(entries_count) = entries_count {
-            entries_count
-        } else {
-            self.config.default_entries_count
-        };
-
-        let mut pools = self.pools.write().unwrap();
-        pools.insert(
-            table_id,
-            Arc::new(BufferPoolImpl::new(page_size, entries_count)),
-        );
+    fn contains_unique_buffer_pool(&self, table_id: TableId) -> bool {
+        let unique_buffer_pools_read_lock = self.unique_buffer_pools.read();
+        unique_buffer_pools_read_lock.contains_key(&table_id)
     }
 
     fn get_buffer_pool(&self, table_id: TableId) -> Arc<dyn BufferPool> {
-        let pools = self.pools.read().unwrap();
-        let buffer_pool = pools.get(&table_id).unwrap();
-        Arc::clone(buffer_pool) as Arc<dyn BufferPool>
+        let unique_buffer_pools_read_lock = self.unique_buffer_pools.read();
+
+        let buffer_pool =
+            if let Some(unique_buffer_pool) = unique_buffer_pools_read_lock.get(&table_id) {
+                Arc::clone(&unique_buffer_pool)
+            } else {
+                Arc::clone(&self.default_buffer_pool)
+            };
+
+        buffer_pool
     }
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn api_test_without_load() {
-        let buffer_pool_manager_config = BufferPoolManagerConfig {
-            default_page_size: 16,
-            default_entries_count: 8,
-        };
-
-        let person_table_id = TableId(String::from("Person"));
-        let phone_number_table_id = TableId(String::from("PhoneNumber"));
-
-        let page0_id = PageId(0);
-
-        let buffer_pool_manager: Arc<dyn BufferPoolManager> =
-            Arc::new(BufferPoolManagerImpl::new(buffer_pool_manager_config));
-
-        buffer_pool_manager.create_buffer_pool(person_table_id.clone(), None, None);
-
-        assert!(buffer_pool_manager.contains_buffer_pool(person_table_id.clone()));
-        assert!(!buffer_pool_manager.contains_buffer_pool(phone_number_table_id.clone()));
-
-        let person_buffer_pool = buffer_pool_manager.get_buffer_pool(person_table_id.clone());
-
-        buffer_pool_manager.create_buffer_pool(phone_number_table_id.clone(), Some(8), Some(16));
-
-        assert!(buffer_pool_manager.contains_buffer_pool(person_table_id.clone()));
-        assert!(buffer_pool_manager.contains_buffer_pool(phone_number_table_id.clone()));
-
-        assert!(!person_buffer_pool.contains_page(page0_id.clone()));
+    fn create_unique_buffer_pool(&self, table_id: TableId, page_size: usize, entries_count: usize) {
+        let mut unique_buffer_pools_write_lock = self.unique_buffer_pools.write();
+        unique_buffer_pools_write_lock.insert(
+            table_id,
+            Arc::new(BufferPoolImpl::new(page_size, entries_count)),
+        );
     }
 }
--- src/src/shared/mod.rs
+++ src/src/shared/mod.rs
@@ -1,3 +1,2 @@
 pub mod config;
-pub mod contracts;
 pub mod definitions;
